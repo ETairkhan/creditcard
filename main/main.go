165f1e4aa53e43eb768a61df63675220431a7ed2
tayermek <trouzerop@gmail.com>
2025-01-11 14:49:30 +0500

    generate, information, issue, main , validate packages

---
+++ main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello")
+}
+creditcard/
+├── main.go
+├── validate.go
+├── generate.go
+├── information.go
+├── issue.go
+├── brands.txt
+└── issuers.txt
