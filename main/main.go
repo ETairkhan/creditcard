41ca6c1f093d23a53246b7d034cb7f8621cead41
tayermek <trouzerop@gmail.com>
2025-01-13 15:02:02 +0500

    generate in main now

--- main/main.go
+++ main/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"creditcard/generate"
 	"creditcard/validate"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -24,9 +26,25 @@ func main() {
 			if err := scanner.Err(); err != nil {
 				panic(err)
 			}
-		}else{
+		} else {
 			validate.Validate(args[1:])
 		}
 	} else if len(args) > 0 && args[0] == "generate" {
+		// Generation logic
+		if len(args) < 2 {
+			fmt.Println("Usage: generate <card_number_pattern> [--pick]")
+			os.Exit(1)
+		}
+
+		// Detect --pick flag
+		pick := false
+		cardPattern := args[1]
+		if len(args) > 2 && args[1] == "--pick" {
+			pick = true
+			cardPattern = args[2]
+		}
+
+		// Call the generate function
+		generate.GenerateNumbers(cardPattern, pick)
 	}
 }
